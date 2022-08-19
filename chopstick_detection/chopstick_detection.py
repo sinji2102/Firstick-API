30a72472a5b54f3a17f05c9bdb647fd52a6d2618
sinji2102 <sinzi031012@naver.com>
2022-08-19 16:00:57 +0900

    Fix: 허프 변환 변경 (표준 > 확률적)

--- chopstick_detection/chopstick_detection.py
+++ chopstick_detection/chopstick_detection.py
@@ -10,29 +10,19 @@ while True :
 
     dst = resize.copy()
     gray = cv.cvtColor(resize, cv.COLOR_BGR2GRAY)
-    canny = cv.Canny(gray, 5000, 1500, apertureSize = 5, L2gradient = False)
+    blur = cv.GaussianBlur(gray, (3, 3), 0)
+    canny = cv.Canny(blur, 1000, 2000, apertureSize = 5, L2gradient = True)
 
-    line = cv.HoughLines(canny, 0.8, np.pi / 180 , 150, srn = 100, stn = 200, min_theta = 0, max_theta = np.pi)
+    line = cv.HoughLinesP(canny, 0.8, np.pi / 180 , 90, minLineLength = 10, maxLineGap = 100)
 
     if line is not None :
-        # 표준 허프 변환
+        # 확률적 허프 변환
         for i in line :
-            rho, theta = i[0][0], i[0][1]
-            a, b = np.cos(theta), np.sin(theta)
-            x0, y0 = a*rho, b*rho
-
-            scale = frame.shape[0] + frame.shape[1]
-
-            x1 = int(x0 + scale * -b)
-            y1 = int(y0 + scale * a)
-            x2 = int(x0 - scale * -b)
-            y2 = int(y0 - scale * a)
-
-            cv.line(dst, (x1, y1), (x2, y2), (0,0,255), 2)
-            cv.circle(dst, (x0, y0), 3, (255, 0, 0), 5, cv.FILLED)
-
+            cv.line(dst, (int(i[0][0]), int(i[0][0])), (int(i[0][2]), int(i[0][3])), (0, 0, 255), 2)
 
     cv.imshow("resize", resize)
+    # cv.imshow("gray", gray)
+    cv.imshow("canny", canny)
     cv.imshow("dst", dst)
 
     key = cv.waitKey(10)
