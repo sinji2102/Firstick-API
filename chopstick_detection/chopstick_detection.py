1b6f4aaa852b050c44cc39701fbb24914d3be3ec
sinji2102 <sinzi031012@naver.com>
2022-08-28 19:24:02 +0900

    Feat: 점 찍는 기능 추가 및 젓가락 일부 blur, canny 추가

--- chopstick_detection/chopstick_detection.py
+++ chopstick_detection/chopstick_detection.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-cap = cv.VideoCapture("C:/Users/sinji/Desktop/firstick/src/iron_wrong01.mp4")
+cap = cv.VideoCapture("C:/Users/sinji/Desktop/firstick/src/wood01.mp4")
 cnt = 1
 
 # 젓가락 전체 길이와 젓가락 쥔 앞 부분 길이 확인해서 1/5 지점 이면 True를 출력하는 코드
@@ -13,36 +13,43 @@ while True :
     dst = resize.copy()
     gray = cv.cvtColor(resize, cv.COLOR_BGR2GRAY)
     blur = cv.GaussianBlur(gray, (3, 3), 0)
+    blur_ = cv.GaussianBlur(gray, (5, 5), 0)
     canny = cv.Canny(blur, 800, 2000, apertureSize = 5, L2gradient = True)
+    canny_ = cv.Canny(blur_, 800, 2000, apertureSize = 5, L2gradient = True)
 
     # 젓가락 전체
     line = cv.HoughLinesP(canny, 0.2, np.pi / 180 , 50, minLineLength = 400, maxLineGap = 150)
 
     # 젓가락 일부
-    lines = cv.HoughLinesP(canny, 0.2, np.pi / 180, 50, minLineLength = 150, maxLineGap = 55)
+    lines = cv.HoughLinesP(canny_, 0.2, np.pi / 180, 50, minLineLength = 150, maxLineGap = 40)
 
     whole = 0
     part = 0
 
+    a11, a12, a21, a22, b11, b12, b21, b22 = 0, 0, 0, 0, 0, 0, 0, 0
+
     # 젓가락 전체 출력 / 빨간색
     if line is not None :
         for i in line :
-            (a1, b1, a2, b2) = i[0]
-            if abs(b1 - b2) < 70 :
-                cv.line(resize, (a1, b1), (a2, b2), (0, 0, 255), 2)
-                whole = a2 - a1
+            (a11, b11, a12, b12) = i[0]
+            if abs(b11 - b12) < 70 :
+                cv.line(resize, (a11, b11), (a12, b12), (0, 0, 255), 2)
+                whole = a12 - a11
 
     # 젓가락 일부(손가락 부분 제거하고) 출력 / 노란색
     if lines is not None :
         for i in lines :
-            (a1, b1, a2, b2) = i[0]
-            if abs((b2 - b1)/(a2 - a1)) < 5 :  # 각도 왜 안 돼??
-                # cv.line(resize, (a1, b1), (a2, b2), (0, 255, 255), 2)
-                part = a2 - a1
-    
-    if whole * 0.5 < part < whole * 0.6 :
-        print("True", cnt)
-        cnt += 1
+            (a21, b21, a22, b22) = i[0]
+            if abs((b22 - b21)/(a22 - a21)) < 5 :  # 각도 왜 안 돼??
+                # cv.line(resize, (a21, b21), (a22, b22), (0, 255, 255), 2)
+                part = a22 - a21
+
+    # 점 찍기
+    if lines is not None :
+        if whole * 0.6 < part < whole * 0.7 :
+            print("True", cnt)
+            cnt += 1
+            cv.line(resize, (a22, b22), (a22, b22), (255, 0, 0), 15)
 
     cv.imshow("resize", resize)
     cv.imshow("canny", canny)
