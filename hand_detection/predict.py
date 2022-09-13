ad6c07e302c4f6603cfd352dfea78007ccd54e50
ShimHyerin <nier8702@kookmin.ac.kr>
2022-09-13 11:17:30 +0900

    Feat: Predict col data 추가

--- hand_detection/predict.py
+++ hand_detection/predict.py
@@ -50,13 +50,12 @@ with mp_hands.Hands(
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     try:
-      # print('try')
-
       hand = results.multi_hand_landmarks
       a = [landmarks.landmark for landmarks in hand]
       b = [data for data in a]
 
-      hand_row = [] 
+      hand_row = []
+      x = []
       y = []
       z = []
       for landmarks in hand:
@@ -64,30 +63,48 @@ with mp_hands.Hands(
             hand_row.append(data.x)
             hand_row.append(data.y)
             hand_row.append(data.z)
+            x.append(np.round(data.x, 5))
             y.append(np.round(data.y, 5))
             z.append(np.round(data.z, 5))
-
-          # Draw hand line 
           mp_drawing.draw_landmarks(
               image, landmarks, mp_hands.HAND_CONNECTIONS)
 
+      hand_row += [y[16]-y[12],y[15]-y[11], y[16]-y[8], y[15]-y[7]]
+      def tmp(ip):
+        return np.round(ip, 4)
+
+      # debugging
+      print(tmp(y[16]-y[12]),tmp(y[15]-y[11]), tmp(y[16]-y[8]), tmp(y[15]-y[7]))
+      print(tmp(x[16]-x[12]))
+
       # predict
       X = pd.DataFrame([hand_row])
-      hand_class = model.predict(X)[0]
-      hand_prob = model.predict_proba(X)[0]
+      hand_class = model.predict(X)[0] # prednp
+      hand_prob = np.max(model.predict_proba(X)[0])
+
 
       # 결과 출력
-      print(hand_class, np.max(hand_prob))
-      cv2.putText(image, hand_class, (200, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255),thickness=3)
+      print(hand_class, hand_prob) # debugging
+      if hand_prob > 0.75:
+        if hand_prob <= 0.89: 
+          # print('Hmm')
+          if hand_class == 'open':
+            cv2.putText(image, 'close', (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255),thickness=3)
+          else: 
+            cv2.putText(image, 'open', (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255),thickness=3)
 
-
-
+        else: 
+          # print(hand_class)
+          cv2.putText(image, hand_class, (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255),thickness=3)
+      else: 
+          cv2.putText(image, 'Hold the chopsticks.', (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255),thickness=3)
+      cv2.putText(image, hand_class+' / '+str(hand_prob)+'%', (200, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255),thickness=3)
 
     except:
       # print('except')
       pass
 
-    cv2.imshow('test', image)
+    cv2.imshow('predict_test', image)
     if cv2.waitKey(10) & 0xFF == ord('q'):
       break
 
