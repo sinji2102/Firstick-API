ed1673152b214da3baf10a474f98ffdd369829bb
sinji2102 <sinzi031012@naver.com>
2022-11-15 10:05:00 +0900

    Feat: 영상 resize

--- open_close/open_close.py
+++ open_close/open_close.py
@@ -5,6 +5,14 @@ cap = cv.VideoCapture("src/wood05.mp4")
 
 while True :
 
+    ret, frame = cap.read()
+    resize = cv.resize(frame, (0,0), fx = 0.5, fy = 0.5, interpolation = cv.INTER_AREA)
+
+
+
+
+    cv.imshow("원본", resize)
+
     key = cv.waitKey(5)
     if key == 27 :
         break
